ac4749075f7fa3206c62e1d831efb851a4b4b868
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-08-14 11:23:04 +0200

    Advanced options components

--- src/cljs/db_quiz/components.cljs
+++ src/cljs/db_quiz/components.cljs
@@ -1,53 +1,49 @@
 (ns db-quiz.components
   (:require [db-quiz.layout :refer [hex-triangle]]
-            [db-quiz.logic :refer [make-a-guess]]
+            [db-quiz.logic :refer [init-board make-a-guess]]
             [db-quiz.model :as model] 
             [db-quiz.state :refer [app-state]]
-            [reagent.core :as reagent]
-            [reagent-forms.core :refer [bind-fields]]))
+            [db-quiz.util :refer [join-by-space redirect toggle]]
+            [db-quiz.modals :as modals]
+            [clojure.string :as string]
+            [reagent.core :refer [atom]]
+            [reagent-forms.core :refer [bind-fields]]
+            [reagent-modals.modals :as reagent-modals]))
+
+(defn parse-hash
+  [h]
+  (let [kv-pairs (-> h
+                     (string/replace #"^#\?" "")
+                     (string/split #"&"))
+        split-fn (fn [kv-pair] (string/split kv-pair #"=" 2))]
+    (into {}
+          (map (comp (juxt (comp keyword first) second) split-fn)
+               kv-pairs))))
+
+(defn set-hash-options!
+  []
+  (let [location (.-location js/window)
+        h (js/decodeURIComponent (.-hash location))]
+    (when h
+      (when-let [doc-url (:doc (parse-hash h))]
+        ; TODO: Refactor
+        (swap! app-state (comp #(assoc-in % [:options :doc] (js/decodeURIComponent doc-url))
+                               #(assoc-in % [:options :data-source] :gdrive)
+                               #(assoc-in % [:options :share-url] (.-href location))))))))
 
 ; ----- Common components -----
 
 (defn label-element
   [for-id text]
-  [:label.col-sm-2.control-label {:for for-id} text])
+  [:label.col-sm-4.control-label {:for for-id} text])
+
+(def info-button
+  [:button#info-button.btn.btn-default {:on-click #(reagent-modals/modal! modals/game-info
+                                                                          {:size :lg})
+                                        :title "O hře"}
+   [:span.glyphicon.glyphicon-info-sign]])
 
 ; ----- Home page -----
-
-(defn player-form-field
-  [id label]
-  (let [local-id (keyword (str "players." id))]
-    [:div.form-group [label-element local-id label] 
-                     [:div.col-sm-10
-                       [:input.form-control {:field :text :id local-id :max-length 20 :type "text"}]
-                       [:div.alert.alert-danger
-                         {:field :alert :id local-id :event empty?}
-                         "Jméno hráče musí být vyplněno!"]]]))
-
-(def difficulty-picker
-  [:div.form-group
-   [label-element "data.difficulty" "Obtížnost"]
-   [:div.btn-group.col-sm-10 {:field :single-select :id :data.difficulty :role "group"}
-    [:button.btn.btn-default {:key :easy} "Jednoduchá"]
-    [:button.btn.btn-default {:key :normal} "Běžná"]
-    [:button.btn.btn-default {:key :hard} "Vysoká"]]])
-
-(def start-menu
-  [:div#start-menu.form-horizontal
-    (player-form-field "player-1" "1. hráč")
-    (player-form-field "player-2" "2. hráč")
-    difficulty-picker
-    [:a.button {:href "/#play"} [:span "Hrát"]]])
-
-(defn home-page
-  []
-  [:div.container-fluid
-   [:div#logo [:img {:alt "DB quiz logo"
-                     :src "/img/logo.svg"}]]
-   [:div.col-sm-6.col-sm-offset-3
-    [bind-fields start-menu app-state]]])
-
-; ----- Play page -----
 
 (defn loading-indicator
   []
@@ -56,19 +52,163 @@
      [:div#loadhex]
      [:p.vcenter "Načítání..."]]))
 
-(defn friend-source [text]
-  (filter
-   #(-> % (.toLowerCase %) (.indexOf text) (> -1))
-   ["Alice" "Alan" "Bob" "Beth" "Jim" "Jane" "Kim" "Rob" "Zoe"]))
+(defn player-form-field
+  [id label]
+  (let [local-id (keyword (str "players." id))]
+    [:div {:class (join-by-space "form-group" "player-field" id)}
+     [label-element local-id label] 
+     [:div.col-sm-8
+      [:input.form-control {:field :text :id local-id :max-length 20 :type "text"}]
+      [:div.alert.alert-danger
+       {:field :alert :id local-id :event empty?}
+       "Jméno hráče musí být vyplněno!"]]]))
+
+(def google-spreadsheet-url
+  [:div.form-group
+   [:p [:label.control-label {:for "options.doc"} "Google Spreadsheet URL"]
+         [:a {:on-click #(reagent-modals/modal! modals/google-spreadsheet-help)}
+          [:span.glyphicon.glyphicon-question-sign.glyphicon-end]]]
+   [:p
+    [:input.form-control {:field :text :id :options.doc :type "url"}]]])
+
+(def share-url
+  [:div.form-group
+   [:p [:label.control-label {:for "options.share-url"} "URL hry"]]
+   [:p [:textarea.form-control {:field :text
+                                :id :options.share-url
+                                :readOnly "readOnly"
+                                :rows 4}]]])
+
+(defn select-box
+  [single? id label & buttons]
+  [:div.form-group
+   [label-element id label]
+   [:div.col-sm-8.btn-group {:field (if single? :single-select :multi-select)
+                              :id (keyword id)
+                              :role "group"}
+    (for [{:keys [value label]} buttons]
+      [:button.btn.btn-default {:key value} label])]])
+
+(def single-select
+  (partial select-box true))
+
+(def multi-select
+  (partial select-box false))
+
+(def field-labelling
+  (single-select "options.labels"
+                 "Označování políček"
+                {:label "0-9" :value :numeric}
+                {:label "A-Z" :value :alphabetic}))
+
+(def language-picker
+  (single-select "options.language"
+                 "Jazyk"
+                 {:label "Česky" :value :czech}
+                 {:label "English" :value :english}))
+
+(def difficulty-picker
+  (single-select "options.difficulty"
+                 "Obtížnost"
+                 {:label "Jednoduchá" :value :easy}
+                 {:label "Běžná" :value :normal}
+                 {:label "Vysoká" :value :hard}))
+
+(def class-picker
+  (multi-select "options.classes"
+                "Druhy otázek"
+                {:label "Osoby" :value "http://dbpedia.org/ontology/Person"}
+                {:label "Místa" :value "http://dbpedia.org/ontology/Place"}
+                {:label "Díla" :value "http://dbpedia.org/ontology/Work"}))
+
+(def google-spreadsheet-options
+  [:div google-spreadsheet-url
+        share-url])
+
+(def dbpedia-options
+  [:div language-picker
+        class-picker
+        difficulty-picker
+        field-labelling])
+
+(def advanced-options
+  (let [toggle-data-source (partial toggle [:dbpedia :gdrive])
+        click-handler {:on-click (fn [_] (swap! app-state
+                                                #(update-in %
+                                                            [:options :data-source]
+                                                            toggle-data-source)))}
+        tab-pane-class (partial join-by-space "tab-pane")
+        generate-share-url (fn [url] 
+                             (let [location (.-location js/window)]
+                               (str (.-origin location)
+                                    (.-pathname location)
+                                    "#?doc="
+                                    (js/encodeURIComponent url))))]
+    (fn []
+      (let [[dbpedia-class gdrive-class] (case (get-in @app-state [:options :data-source])
+                                               :dbpedia ["active" ""]
+                                               :gdrive ["" "active"])]
+        [:div
+         [:p "Zvolte zdroj otázek:"]
+         [:ul.nav.nav-tabs 
+          [:li {:class dbpedia-class} [:a click-handler "DBpedia"]]
+          [:li {:class gdrive-class} [:a click-handler "Google Spreadsheet"]]]
+         [:div.tab-content
+          [:div {:class (tab-pane-class dbpedia-class)}
+            [bind-fields dbpedia-options app-state]]
+          [:div {:class (tab-pane-class gdrive-class)}
+            [bind-fields
+             google-spreadsheet-options
+             app-state
+             (fn [id value state]
+               (when (= (vec id) [:options :doc])
+                 (assoc-in state
+                           [:options :share-url]
+                           (generate-share-url value))))]]]]))))
+
+(def basic-options
+  [:div
+   (player-form-field "player-1" "1. hráč")
+   (player-form-field "player-2" "2. hráč")])
+
+(def start-menu
+  (let [options-hidden (atom true)]
+    (set-hash-options!)
+    (fn []
+      [:div.col-sm-6.col-sm-offset-3
+       [:div#start-menu.form-horizontal
+        [bind-fields basic-options app-state]
+        [:div#advanced 
+         [:h4
+          [:a {:on-click (fn [e]
+                           (swap! options-hidden not)
+                           (.preventDefault e))}
+           [:span {:class (join-by-space "glyphicon"
+                                         "glyphicon-start"
+                                         (if @options-hidden
+                                           "glyphicon-chevron-right"
+                                           "glyphicon-chevron-down"))}]
+           "Pokročilé nastavení"]]
+         (when-not @options-hidden
+           [advanced-options])]
+        [:a.button {:on-click #(; TODO: Validate options
+                                model/load-board-data (init-board)
+                                                      (partial redirect "#play"))}
+         [:span "Hrát"]]]])))
+
+(defn home-page
+  []
+  [:div.container-fluid
+   [loading-indicator]
+   info-button
+   [:div#logo [:img {:alt "DB quiz logo"
+                     :src "/img/logo.svg"}]]
+   [start-menu]
+   [reagent-modals/modal-window]])
+
+; ----- Play page -----
 
 (def autocomplete
-  ;[:div {:field           :typeahead
-  ;       :id              :answer
-  ;       :data-source     friend-source
-  ;       :input-class     "form-control"
-  ;       :list-class      "typeahead-list"
-  ;       :item-class      "typeahead-item"
-  ;       :highlight-class "highlighted"}])
   [:input.form-control {:autoFocus "autoFocus"
                         :field :text
                         :id :answer
@@ -102,7 +242,7 @@
     [:div#verdict.row {:class (when (nil? verdict) "transparent")}
       [:div.col-sm-12
         [:p {:class (str "alert " verdict-class)}
-          [:span {:class (str "glyphicon " glyphicon-class)}]
+          [:span {:class (join-by-space "glyphicon" "glyphicon-start" glyphicon-class)}]
           success ". Správná odpověď je " [:strong correct-answer] "."]]]))
 
 (defn question-box
@@ -130,8 +270,9 @@
               [:span.glyphicon.glyphicon-forward]
               " Dál"]]]]]
       [verdict-component]
-      [:div.row
-        [:p.col-sm-12 [:strong "Řešení: "] label]]]))
+      ;[:div.row
+      ;  [:p.col-sm-12 [:strong "Řešení: "] label]]
+      ]))
 
 (defn player-on-turn
   "Show the name of the player, whose turn it currently is."
@@ -155,19 +296,19 @@
   []
   (let [{:keys [current-field]} @app-state]
     [:div.container-fluid
-    [loading-indicator]
-    [:div.row
+     [:div.row
       [:div.col-sm-6 [hex-triangle]]
       [:div#question-box.col-sm-6
-        [player-on-turn]
-        (when current-field 
-          [question-box current-field])]]]))
+       [player-on-turn]
+       (when current-field 
+         [question-box current-field])]]]))
 
 ; ----- End page -----
 
 (defn end-page
   []
   (let [{{:keys [player] :as winner} :winner
+         {:keys [share-url]} :options
          :keys [players]} @app-state
         winner-name (players player)]
     [:div
@@ -175,4 +316,4 @@
        [:div#winner
         [:p "Vítězem se stává"]
         [:h1 {:class (name player)} winner-name]])
-     [:a.button {:href "/"} [:span "Hrát znovu"]]]))
+     [:a.button {:href (or share-url "")} [:span "Hrát znovu"]]]))
