0f0fa6660f20e9804636d6094c7f8668da7351b4
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-08-14 11:22:01 +0200

    Advanced options in app-state

--- src/cljs/db_quiz/state.cljs
+++ src/cljs/db_quiz/state.cljs
@@ -1,16 +1,24 @@
 (ns db-quiz.state
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(def app-state (atom {:answer nil
-                      :board {}
-                      :current-field nil
-                      :data {:classes :default
-                             :difficulty :normal}
-                      :loading? false
-                      :on-turn (rand-nth [:player-1 :player-2])
-                      :players {:player-1 "Dmitrij"
-                                :player-2 "Paní M."}
-                      :timer {:completion 0
-                              :start 0}
-                      :verdict nil
-                      :winner nil}))
+(defonce app-state
+  (atom {:answer nil
+         :board {}
+         :current-field nil
+         :loading? false
+         :on-turn (rand-nth [:player-1 :player-2])
+         :options {:classes ["http://dbpedia.org/ontology/Person"
+                             "http://dbpedia.org/ontology/Place"
+                             "http://dbpedia.org/ontology/Work"]
+                   :data-source :dbpedia
+                   :difficulty :normal
+                   :doc ""
+                   :labels :numeric
+                   :language :czech
+                   :share-url ""}
+         :players {:player-1 "Dmitrij"
+                   :player-2 "Paní M."}
+         :timer {:completion 0
+                 :start 0}
+         :verdict nil
+         :winner nil}))
