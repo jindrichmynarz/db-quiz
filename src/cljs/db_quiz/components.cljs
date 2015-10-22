5ba266dfe72b7172291f1435dce0fca873260631
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-10-22 17:50:11 +0200

    More compatible substring test

--- src/cljs/db_quiz/components.cljs
+++ src/cljs/db_quiz/components.cljs
@@ -574,7 +574,7 @@
 (defn facebook-button
   "Button to send a Facebook status about your game victories."
   [url]
-  (let [app-id (if (.startsWith url "http://localhost") "873059332790811" "873050382791706")]
+  (let [app-id (if (zero? (.indexOf url "http://localhost")) "873059332790811" "873050382791706")]
      [:a.btn-hex {:href (str "https://www.facebook.com/dialog/feed?"
                              (generate-query-string {:app_id app-id
                                                      :display "page"
