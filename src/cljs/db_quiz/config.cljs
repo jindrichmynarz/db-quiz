d96b40badfb7abd733181596aa4b2922f8b4a363
Jindrich Mynarz <mynarzjindrich@gmail.com>
2020-10-22 19:43:37 +0200

    Update SPARQL endpoint URL

--- src/cljs/db_quiz/config.cljs
+++ src/cljs/db_quiz/config.cljs
@@ -19,8 +19,8 @@
              :missed "#333333"
              :player-1 "#fc4349"
              :player-2 "#354d65"}
-   :data {:sparql {:endpoint-urls {:cs "https://doc.lmcloud.vse.cz:8443/sparql"
-                                   :en "https://doc.lmcloud.vse.cz:8443/sparql"}
+   :data {:sparql {:endpoint-urls {:cs "https://xtest.vse.cz/virtuoso-dbquiz/sparql"
+                                   :en "https://xtest.vse.cz/virtuoso-dbquiz/sparql"}
                    :query-files {:cs {:alphabetic "sparql/cs_dbpedia_az.mustache"
                                       :numeric "sparql/cs_dbpedia.mustache"}
                                  :en {:alphabetic ""
